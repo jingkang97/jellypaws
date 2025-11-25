13e77aafae11203d8c0572e526c19bfcd2c7bbf2
jingkang97 <jingkang@lomotif.com>
2025-11-25 22:03:37 +0800

    test gyroscope 5

--- src/components/FallingGachapon/OrientationTesting.tsx
+++ src/components/FallingGachapon/OrientationTesting.tsx
@@ -1,34 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDeviceOrientation } from "./useDeviceOrientation";
-import ToggleButton from "./Toggle";
 
 const OrientationInfo = (): React.ReactElement => {
   const { orientation, requestAccess, revokeAccess, error } =
     useDeviceOrientation();
 
-  const handleToggle = async (isOn: boolean): Promise<void> => {
-    if (isOn) {
-      // Turn on - request access
-      const granted = await requestAccess();
-      if (!granted) {
-        console.error("Failed to get device orientation permission");
-      }
-    } else {
-      // Turn off - revoke access
-      await revokeAccess();
-    }
-  };
+  // const onToggle = (toggleState: boolean): void => {
+  //   const result = toggleState ? requestAccess() : revokeAccess();
+  // };
+
+  useEffect(() => {
+    return () => {
+      requestAccess();
+    };
+  }, []);
 
   const orientationInfo = orientation && (
     <ul>
       <li>
-        ɑ: <code>{orientation.alpha?.toFixed(2) ?? "N/A"}</code>
+        ɑ: <code>{orientation.alpha}</code>
       </li>
       <li>
-        β: <code>{orientation.beta?.toFixed(2) ?? "N/A"}</code>
+        β: <code>{orientation.beta}</code>
       </li>
       <li>
-        γ: <code>{orientation.gamma?.toFixed(2) ?? "N/A"}</code>
+        γ: <code>{orientation.gamma}</code>
       </li>
     </ul>
   );
@@ -39,11 +35,7 @@ const OrientationInfo = (): React.ReactElement => {
 
   return (
     <>
-      <ToggleButton
-        onToggle={handleToggle}
-        labelOn="Disable Gyroscope"
-        labelOff="Enable Gyroscope"
-      />
+      {/* <Toggle onToggle={onToggle} /> */}
       {orientationInfo}
       {errorElement}
     </>
